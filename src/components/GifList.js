ca4ebed781ed9109db73505aed13b2d511e2bb5d
ACAPP-dev <acapp909@gmail.com>
2020-07-07 17:35:07 -0700

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,18 +3,13 @@ import React from 'react'
 export default class GifList extends React.Component {
 
     render() {
-        return (
+       return (
         <ul>
-            <li>
-                <img src={this.props.data.images[0]} alt="" />
-            </li>
-            <li>
-                <img src={this.props.data.images[1]} alt="" />
-            </li>
-            <li>
-                <img src={this.props.data.images[2]} alt="" />
-            </li>
+            {this.props.data.images.map(image => {
+                return (<li>
+                    <img src={image} alt="" />
+                </li>)
+            })}
         </ul>
-        )
-    }
+       )}
 }
